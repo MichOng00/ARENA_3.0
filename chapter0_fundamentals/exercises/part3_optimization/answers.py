a8de5983cb7a8135e3f9f61f0702010039de4c30
Michelle Ong <michelleongshutian@gmail.com>
2025-12-13 15:48:14 +0800

    wandb hyperparameter sweep exercise

--- chapter0_fundamentals/exercises/part3_optimization/answers.py
+++ chapter0_fundamentals/exercises/part3_optimization/answers.py
@@ -687,3 +687,54 @@ args = WandbResNetFinetuningArgs()
 trainer = WandbResNetFinetuner(args)
 trainer.train()
 # %%
+### WandB hyperparameter sweep
+# random (vs grid) gives more distinct values in the important hyperparams
+sweep_config = dict(
+    method = "random",
+    metric = dict(name="accuracy", goal="maximize"),
+    parameters = dict(
+        learning_rate = dict(min=1e-4, max=1e-1, distribution="log_uniform_values"),
+        batch_size = dict(values=[32, 64, 128, 256]),
+        weight_decay = dict(min=1e-4, max=1e-2, distribution="log_uniform_values"),
+        weight_decay_bool = dict(values=[True, False]),
+    ),
+)
+
+
+def update_args(
+    args: WandbResNetFinetuningArgs, sampled_parameters: dict
+) -> WandbResNetFinetuningArgs:
+    """
+    Returns a new args object with modified values. The dictionary `sampled_parameters` will have
+    the same keys as your `sweep_config["parameters"]` dict, and values equal to the sampled values
+    of those hyperparameters.
+    """
+    assert set(sampled_parameters.keys()) == set(sweep_config["parameters"].keys())
+
+    args.learning_rate = sampled_parameters["learning_rate"]
+    args.batch_size = sampled_parameters["batch_size"]
+    args.weight_decay = sampled_parameters["weight_decay"] if sampled_parameters["weight_decay_bool"] else 0.0
+
+    return args
+
+
+tests.test_sweep_config(sweep_config)
+tests.test_update_args(update_args, sweep_config)
+# %%
+def train():
+    # Define args & initialize wandb
+    args = WandbResNetFinetuningArgs()
+    wandb.init(project=args.wandb_project, name=args.wandb_name, reinit=False)
+
+    # After initializing wandb, we can update args using `wandb.config`
+    args = update_args(args, dict(wandb.config))
+
+    # Train the model with these new hyperparameters (the second `wandb.init` call will be ignored)
+    trainer = WandbResNetFinetuner(args)
+    trainer.train()
+
+
+sweep_id = wandb.sweep(sweep=sweep_config, project="day3-resnet-sweep")
+wandb.agent(sweep_id=sweep_id, function=train, count=3)
+wandb.finish()
+# %%
